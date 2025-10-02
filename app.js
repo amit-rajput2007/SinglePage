541066a923d8d844dd058569df012a9c754104aa
arajput3 <amit_rajput@intuit.com>
2025-10-02 13:51:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var clientId = "3MVG9XgkMlifdwVAPP1QzilrWxN4vxXanUxrP_wtiJKJMqA6GnZ1u41eQ.oP_8lPeh80GlG4byc0QpN3U2.84";
 var baseURL = "https://tiger-dev-ed.trailblaze.my.site.com";
 var redirectURL = "https://tiger-dev-ed.trailblaze.my.site.com/services/apexrest/code/extraction"
-
+var client_secret="6718620B8A4513CE318C18A789951D47EB0B18E931298F428B9C90F10DE44B56";
 function getToken(response){
 
     var xttreq= new XMLHttpRequest();
@@ -31,7 +31,7 @@ xhttprequest.open("POST",baseURL+"/services/oauth2/authorize",true);
 xhttprequest.setRequestHeader("Auth-Request-Type","Named-User");
 xhttprequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 xhttprequest.setRequestHeader("Authorization","Basic "+cred);
-xhttprequest.send("response_type=code_credentials&client_id="+clientId+"&redirect_uri="+redirectURL);
+xhttprequest.send("response_type=code_credentials&client_id="+clientId+"client_secret"++"&redirect_uri="+redirectURL);
 xhttprequest.onreadystatechange=function (){
 if(this.readyState==3){
 var response=JSON.parse(xhttprequest.response);
@@ -41,7 +41,6 @@ console.log('sfdc_community_url'+response.code);
 getToken(response);
 }
 }
-    
 }
 
 // Attach the event listener to the form
