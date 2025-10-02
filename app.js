c3d72cf6db5de9c786a39e6f76f3c21efcb299e6
arajput3 <amit_rajput@intuit.com>
2025-10-01 21:59:36 -0600

    singlepage

---
+++ app.js
@@ -0,0 +1,32 @@
+
+var clientId = "3MVG9XgkMlifdwVAPP1QzilrWxN4vxXanUxrP_wtiJKJMqA6GnZ1u41eQ.oP_8lPeh80GlG4byc0QpN3U2.84";
+var baseURL = "	https://tiger-dev-ed.trailblaze.my.site.com";
+var redirectURL = "	https://tiger-dev-ed.trailblaze.my.site.com/services/apexrest/code/extraction"
+
+
+function startLogin(event) {
+    // Prevent the form's default submit behavior
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log('username ' + username);
+    console.log('password ' + password);
+var cred=btoa(username+":"+password);
+var xhttprequest= new XMLHttpRequest();
+xhttprequest.open("POST",baseURL+"/services/oauth2/authorize",true);
+xhttprequest.setRequestHeader("Auth-Request-Type","Named-User");
+xhttprequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+xhttprequest.setRequestHeader("Authorization","Basic "+cred);
+xhttprequest.send("response_type=code_credentials&client_id="+clientId+"&redirect_uri="+redirectURL);
+xhttprequest.onreadystatechange=function (){
+if(this.readyState==3){
+var response=JSON.parse(xhttprequest.response);
+console.log(response);
+}
+}
+    
+}
+
+// Attach the event listener to the form
+document.getElementById('loginForm').addEventListener('submit', startLogin);
